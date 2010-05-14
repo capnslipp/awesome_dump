2192b95584519c5ff5c2d5ba562dea8e1e339d93
Slippy Douglas <git@6bitt.com>
2010-05-14 16:25:24 -0700

    Fixed up specs to output what I would expect. Removed :multiline and :plain option tests in favor a new :escape option. Almost all specs currently fail.

--- spec/awesome_dump_spec.rb
+++ spec/awesome_dump_spec.rb
@@ -6,277 +6,147 @@ describe "AwesomeDump" do
   before(:each) do
     stub_dotfile!
   end
-
+  
   describe "Array" do
     before(:each) do
       @arr = [ 1, :two, "three", [ nil, [ true, false] ] ]
     end
-
+    
     it "empty array" do
-      [].ad.should == "[]"
+      [].ad.should == []
     end
-
-    it "plain multiline" do
-      @arr.ad(:plain => true).should == <<-EOS.strip
-[
-    [0] 1,
-    [1] :two,
-    [2] "three",
-    [3] [
-        [0] nil,
-        [1] [
-            [0] true,
-            [1] false
-        ]
-    ]
-]
-EOS
-      end
-
-    it "plain multiline indented" do
-      @arr.ad(:plain => true, :indent => 2).should == <<-EOS.strip
-[
-  [0] 1,
-  [1] :two,
-  [2] "three",
-  [3] [
-    [0] nil,
-    [1] [
-      [0] true,
-      [1] false
-    ]
-  ]
-]
-EOS
+    
+    it "default" do
+      @arr.ad.should == [ 1, :two, "three", [ nil, [ true, false ] ] ]
     end
-
-    it "plain single line" do
-      @arr.ad(:plain => true, :multiline => false).should == '[ 1, :two, "three", [ nil, [ true, false ] ] ]'
+    
+    it "safe-escaped" do
+      @arr.ad(:escape => :safe).should == [1, ':two', "three", [nil, [true, false]]]
     end
-
-    it "colored multiline (default)" do
-      @arr.ad.should == <<-EOS.strip
-[
-\e[1;37m    [0] \e[0m\e[1;34m1\e[0m,
-\e[1;37m    [1] \e[0m\e[0;36m:two\e[0m,
-\e[1;37m    [2] \e[0m\e[0;33m\"three\"\e[0m,
-\e[1;37m    [3] \e[0m[
-\e[1;37m        [0] \e[0m\e[1;31mnil\e[0m,
-\e[1;37m        [1] \e[0m[
-\e[1;37m            [0] \e[0m\e[1;32mtrue\e[0m,
-\e[1;37m            [1] \e[0m\e[1;31mfalse\e[0m
-        ]
-    ]
-]
-EOS
-      end
-
-    it "colored multiline indented" do
-      @arr.ad(:indent => 8).should == <<-EOS.strip
-[
-\e[1;37m        [0] \e[0m\e[1;34m1\e[0m,
-\e[1;37m        [1] \e[0m\e[0;36m:two\e[0m,
-\e[1;37m        [2] \e[0m\e[0;33m\"three\"\e[0m,
-\e[1;37m        [3] \e[0m[
-\e[1;37m                [0] \e[0m\e[1;31mnil\e[0m,
-\e[1;37m                [1] \e[0m[
-\e[1;37m                        [0] \e[0m\e[1;32mtrue\e[0m,
-\e[1;37m                        [1] \e[0m\e[1;31mfalse\e[0m
-                ]
-        ]
-]
-EOS
-    end
-
-    it "colored single line" do
-      @arr.ad(:multiline => false).should == "[ \e[1;34m1\e[0m, \e[0;36m:two\e[0m, \e[0;33m\"three\"\e[0m, [ \e[1;31mnil\e[0m, [ \e[1;32mtrue\e[0m, \e[1;31mfalse\e[0m ] ] ]"
+    
+    it "quote-escape" do
+      @arr.ad(:escape => :quote).should == ['1', ':two', '"three"', ['nil', ['true', 'false']]]
     end
   end
-
+  
   #------------------------------------------------------------------------------
   describe "Nested Array" do
     before(:each) do
       @arr = [ 1, 2 ]
       @arr << @arr
     end
-
-    it "plain multiline" do
-      @arr.ad(:plain => true).should == <<-EOS.strip
-[
-    [0] 1,
-    [1] 2,
-    [2] [...]
-]
-EOS
+    
+    it "default" do
+      d = [1, 2, ['...']]
+      @arr.ad.should == d
     end
-
-    it "plain single line" do
-      @arr.ad(:plain => true, :multiline => false).should == "[ 1, 2, [...] ]"
+    
+    it "safe-escaped" do
+      d = [1, 2, ['...']]
+      @arr.ad(:escape => :safe).should == d
+    end
+    
+    it "quote-escape" do
+      d = ['1', '2', ['...']]
+      @arr.ad(:escape => :quote).should == d
     end
   end
-
+  
   #------------------------------------------------------------------------------
   describe "Hash" do
     before(:each) do
-      @hash = { 1 => { :sym => { "str" => { [1, 2, 3] => { { :k => :v } => Hash } } } } }
+      @hash = { 1 => { :sym => { "str" => { [1, 2, 3] => { {:k => :v} => Hash } } } } }
     end
     
     it "empty hash" do
-      {}.ad.should == "{}"
+      {}.ad.should == {}
     end
     
-    it "plain multiline" do
-      @hash.ad(:plain => true).should == <<-EOS.strip
-{
-    1 => {
-        :sym => {
-            "str" => {
-                [ 1, 2, 3 ] => {
-                    { :k => :v } => Hash < Object
-                }
-            }
-        }
-    }
-}
-EOS
+    it "default" do
+      d = { 1 => { :sym => { "str" => { [1, 2, 3] => { {:k => :v} => Hash } } } } }
+      @hash.ad.should == d
     end
-
-    it "plain multiline indented" do
-      @hash.ad(:plain => true, :indent => 1).should == <<-EOS.strip
-{
- 1 => {
-  :sym => {
-   "str" => {
-    [ 1, 2, 3 ] => {
-     { :k => :v } => Hash < Object
-    }
-   }
-  }
- }
-}
-EOS
+    
+    it "safe-escaped" do
+      d = { 1 => { ':sym' => { "str" => { [1, 2, 3] => { {':k' => ':v'} => 'Hash' } } } } }
+      @hash.ad(:escape => :safe).should == d
     end
-
-    it "plain single line" do
-      @hash.ad(:plain => true, :multiline => false).should == '{ 1 => { :sym => { "str" => { [ 1, 2, 3 ] => { { :k => :v } => Hash < Object } } } } }'
+    
+    it "quote-escape" do
+      d = { '1' => { ':sym' => { '"str"' => { ['1', '2', '3'] => { {':k' => ':v'} => 'Hash' } } } } }
+      @hash.ad(:escape => :quote).should == d
     end
-
-    it "colored multiline (default)" do
-      @hash.ad.should == <<-EOS.strip
-{
-    1\e[0;37m => \e[0m{
-        :sym\e[0;37m => \e[0m{
-            \"str\"\e[0;37m => \e[0m{
-                [ 1, 2, 3 ]\e[0;37m => \e[0m{
-                    { :k => :v }\e[0;37m => \e[0m\e[1;33mHash < Object\e[0m
-                }
-            }
-        }
-    }
-}
-EOS
-    end
-
-    it "colored multiline indented" do
-      @hash.ad(:indent => 2).should == <<-EOS.strip
-{
-  1\e[0;37m => \e[0m{
-    :sym\e[0;37m => \e[0m{
-      \"str\"\e[0;37m => \e[0m{
-        [ 1, 2, 3 ]\e[0;37m => \e[0m{
-          { :k => :v }\e[0;37m => \e[0m\e[1;33mHash < Object\e[0m
-        }
-      }
-    }
-  }
-}
-EOS
-    end
-
-    it "colored single line" do
-      @hash.ad(:multiline => false).should == "{ 1\e[0;37m => \e[0m{ :sym\e[0;37m => \e[0m{ \"str\"\e[0;37m => \e[0m{ [ 1, 2, 3 ]\e[0;37m => \e[0m{ { :k => :v }\e[0;37m => \e[0m\e[1;33mHash < Object\e[0m } } } } }"
-    end
-
+    
   end
-
+  
   #------------------------------------------------------------------------------
   describe "Nested Hash" do
     before(:each) do
       @hash = {}
       @hash[:a] = @hash
     end
-
-    it "plain multiline" do
-      @hash.ad(:plain => true).should == <<-EOS.strip
-{
-    :a => {...}
-}
-EOS
+    
+    it "default" do
+      d = { :a => {'...' => '...'} }
+      @hash.ad.should == d
     end
-
-    it "plain single line" do
-      @hash.ad(:plain => true, :multiline => false).should == '{ :a => {...} }'
+    
+    it "safe-escaped" do
+      d = { ':a' => {'...' => '...'} }
+      @hash.ad(:escape => :safe).should == d
     end
-  end
-
-  #------------------------------------------------------------------------------
-  describe "Negative options[:indent]" do
-    before(:each) do
-      @hash = { [0, 0, 255] => :yellow, :red => "rgb(255, 0, 0)", "magenta" => "rgb(255, 0, 255)" }
-    end
-
-    it "hash keys must be left aligned" do
-      out = @hash.ad(:plain => true, :indent => -4)
-      out.start_with?("{\n").should == true
-      out.include?('    :red          => "rgb(255, 0, 0)"').should == true
-      out.include?('    "magenta"     => "rgb(255, 0, 255)"').should == true
-      out.include?('    [ 0, 0, 255 ] => :yellow').should == true
-      out.end_with?("\n}").should == true
+    
+    it "quote-escape" do
+      d = { ':a' => {'...' => '...'} }
+      @hash.ad(:escape => :quote).should == d
     end
   end
-
+  
   #------------------------------------------------------------------------------
   describe "Class" do
-    it "shoud show superclass (plain)" do
-      self.class.ad(:plain => true).should == "#{self.class} < #{self.class.superclass}"
-    end
-
-    it "shoud show superclass (color)" do
-      self.class.ad.should == "#{self.class} < #{self.class.superclass}".yellow
+    it "shoud show superclass (default)" do
+      Fixnum.ad.should == 'Fixnum < Integer'
+      Class.ad.should == 'Class < Module'
     end
   end
-
+  
   #------------------------------------------------------------------------------
   describe "File" do
-    it "should display a file (plain)" do
-      File.open(__FILE__, "r") do |f|
-        f.ad(:plain => true).should == "#{f.inspect}\n" << `ls -alF #{f.path}`.chop
+    it "should display a file (default)" do
+      File.open(__FILE__, 'r') do |f|
+        f.ad.should == "#{f.inspect}\n" << `ls -alF #{f.path}`.chop
       end
     end
   end
-
+  
   #------------------------------------------------------------------------------
   describe "Dir" do
-    it "should display a direcory (plain)" do
+    it "should display a directory (default)" do
       Dir.open(File.dirname(__FILE__)) do |d|
-        d.ad(:plain => true).should == "#{d.inspect}\n" << `ls -alF #{d.path}`.chop
+        d.ad.should == "#{d.inspect}\n" << `ls -alF #{d.path}`.chop
       end
     end
   end
-
+  
   #------------------------------------------------------------------------------
   describe "BigDecimal and Rational" do
     it "should present BigDecimal object as Float scalar" do
       big = BigDecimal("2010.4")
-      big.ad(:plain => true).should == "2010.4"
+      
+      # @TODO: which?
+      big.ad.should == 'BigDecimal("2010.4")'
+      big.ad.should == "2010.4"
     end
-
+    
     it "should present Rational object as Float scalar" do
       rat = Rational(2010, 2)
-      rat.ad(:plain => true).should == "1005.0"
+      
+      # @TODO: which?
+      rat.ad.should == "Rational(2010, 2)"
+      rat.ad.should == "1005.0"
     end
   end
-
+  
   #------------------------------------------------------------------------------
   describe "Utility methods" do
     it "should merge options" do
@@ -287,55 +157,39 @@ EOS
       options[:indent].should == 0
     end
   end
-
-
+  
+  
   #------------------------------------------------------------------------------
   describe "Struct" do
     before(:each) do
       @struct = unless defined?(Struct::SimpleStruct)
-        Struct.new("SimpleStruct", :name, :address).new
+        Struct.new('SimpleStruct', :name, :address).new
       else
         Struct::SimpleStruct.new
       end
-      @struct.name = "Herman Munster"
-      @struct.address = "1313 Mockingbird Lane"
+      @struct.name = 'Herman Munster'
+      @struct.address = '1313 Mockingbird Lane'
     end
     
     it "empty struct" do
-      Struct.new("EmptyStruct").ad.should ==  "\e[1;33mStruct::EmptyStruct < Struct\e[0m"
+      Struct.new('EmptyStruct').ad.should ==  "Struct::EmptyStruct < Struct"
     end
     
-    it "plain multiline" do
-      @struct.ad(:plain => true).should == <<-EOS.strip
-{
-    :address => "1313 Mockingbird Lane",
-       :name => "Herman Munster"
-}
-EOS
+    it "default" do
+      d = {:address => '1313 Mockingbird Lane', :name => 'Herman Munster'}
+      @struct.ad.should == d
     end
-
-    it "plain multiline indented" do
-      @struct.ad(:plain => true, :indent => 1).should == <<-EOS.strip
-{
- :address => "1313 Mockingbird Lane",
-    :name => "Herman Munster"
-}
-EOS
+    
+    it "safe-escaped" do
+      d = {':address' => '1313 Mockingbird Lane', ':name' => 'Herman Munster'}
+      @struct.ad(:escape => :safe).should == d
     end
-
-    it "plain single line" do
-      @struct.ad(:plain => true, :multiline => false).should == "{ :address => \"1313 Mockingbird Lane\", :name => \"Herman Munster\" }"
-    end
-
-    it "colored multiline (default)" do
-      @struct.ad.should == <<-EOS.strip
-{
-    :address\e[0;37m => \e[0m\e[0;33m\"1313 Mockingbird Lane\"\e[0m,
-       :name\e[0;37m => \e[0m\e[0;33m\"Herman Munster\"\e[0m
-}
-EOS
+    
+    it "quote-escaped" do
+      d = {':address' => '1313 Mockingbird Lane', ':name' => 'Herman Munster'}
+      @struct.ad(:escape => :quote).should == d
     end
   end
-
-
+  
+  
 end
